b763319669cf4a72b3ecf1f43c144eeab96a6494
amisadman <inboxsadman@gmail.com>
2026-01-18 12:41:47 +0600

    feat: added better auth

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application, Request, Response } from "express";
 import { sendResponse } from "./utils/response";
 import { postRouter } from "./modules/post/post.router";
-import morgan from "morgan"
+import morgan from "morgan";
 import { accessLogStream } from "./middleware/logger";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
 const app: Application = express();
 app.use(express.json());
-app.use(morgan("combined",{stream:accessLogStream}))
+app.use(morgan("combined", { stream: accessLogStream }));
+
+//better auth
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 //post
 app.use("/api/v1/post", postRouter);
